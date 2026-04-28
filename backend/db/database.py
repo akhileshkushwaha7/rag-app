94ab1b139f369fbddbf81904a7a2467825dfc621
akhileshkushwaha7 <71724220+akhileshkushwaha7@users.noreply.github.com>
2026-04-28 16:02:18 -0400

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -44,43 +44,6 @@
 #     async with async_session() as session:
 #         yield session
 
-import os
-import weaviate
-from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-from models.base import Base
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-WEAVIATE_URL = os.getenv("WEAVIATE_URL")
-
-weaviate_client = None
-
-# def init_weaviate():
-#     global weaviate_client
-
-#     try:
-#         weaviate_client = weaviate.connect_to_custom(
-#             http_host="weaviate-service-99kh.onrender.com",
-#             http_port=80,          # ✅ HTTP PORT (NOT 443)
-#             http_secure=False,
-
-#             grpc_host="weaviate-service-99kh.onrender.com",
-#             grpc_port=50051,       # ✅ MUST be different
-#             grpc_secure=False,
-#         )
-
-#         print("✅ Weaviate connected")
-
-#     except Exception as e:
-#         print(f"⚠️ Weaviate connection failed: {e}")
-#         weaviate_client = None
 
 import os
 import weaviate
