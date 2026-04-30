b57a83cda8afc2ef91adfe24ee4313628a2d332c
akhileshkushwaha7 <71724220+akhileshkushwaha7@users.noreply.github.com>
2026-04-29 20:50:26 -0400

    Update api.ts

--- frontend/src/app/lib/api.ts
+++ frontend/src/app/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://rag-app-ai1w.onrender.com",
   withCredentials: true, // for cookies (auth)
 });
